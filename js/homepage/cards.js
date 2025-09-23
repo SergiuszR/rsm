6e2d47b191ae1a74f306c0568acc8607e70750d6
Sergiusz Różycki <sergiuszrozycki@MacBook-Pro-Sergiusz.local>
2025-09-23 17:49:34 +0200

    cards update

--- js/homepage/cards.js
+++ js/homepage/cards.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function() {
     const cards = document.querySelectorAll('.social-update-item-wrapper');
     const container = document.querySelector('.social_update-slider-wrapper');
     
