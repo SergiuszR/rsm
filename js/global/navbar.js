8e36ad6a94f9ed5b6167d0b214ff0c71a7c7dba7
Sergiusz Różycki <sergiuszrozycki@MacBook-Pro-Sergiusz.local>
2025-09-29 11:55:05 +0200

    navbar behaviour change

--- js/global/navbar.js
+++ js/global/navbar.js
@@ -4,33 +4,25 @@ $(document).ready(function() {
     const $navbar = $('[data-element="navbar"]');
     const $banner = $('[data-element="banner"]');
     
-    if ($navbar.length > 0 && $banner.length > 0) {
+    if ($navbar.length > 0) {
         let lastY = 0;
         
         ScrollTrigger.create({
             start: "top top",
             end: "max",
             onUpdate: self => {
-                // Check banner visibility on every scroll update
-                if ($banner.css('display') === 'none') {
-                    // Banner is hidden, remove pinned class and exit
-                    if ($navbar.hasClass('is-pinned')) {
-                        $navbar.removeClass('is-pinned');
-                    }
-                    return;
-                }
-                
                 const currentY = self.scroll();
+                const isBannerVisible = $banner.length > 0 && $banner.css('display') !== 'none';
                 
                 if (currentY > lastY && currentY > 50) {
-                    // Scrolling down
                     if (!$navbar.hasClass('is-pinned')) {
                         $navbar.addClass('is-pinned');
+                        gsap.to($navbar, { y: -$navbar.outerHeight(), duration: 0.3, ease: "power2.out" });
                     }
                 } else if (currentY < lastY) {
-                    // Scrolling up
                     if ($navbar.hasClass('is-pinned')) {
                         $navbar.removeClass('is-pinned');
+                        gsap.to($navbar, { y: 0, duration: 0.3, ease: "power2.out" });
                     }
                 }
                 
