f5bf57315a6f4cd3fa03f725faa4c62cac04bca0
Sergiusz Różycki <sergiuszrozycki@macbook-pro-sergiusz.home>
2025-10-08 09:44:04 +0200

    navbar scroll update

--- dist/js/global/navbar.js
+++ dist/js/global/navbar.js
@@ -6,27 +6,33 @@ $(document).ready(function () {
 
     if ($navbar.length > 0) {
         let lastY = 0;
+        const threshold = 5;
 
         ScrollTrigger.create({
             start: "top top",
             end: "max",
             onUpdate: self => {
                 const currentY = self.scroll();
+                const scrollDiff = currentY - lastY;
                 const isBannerVisible = $banner.length > 0 && $banner.css('display') !== 'none';
 
-                if (currentY > lastY && currentY > 50) {
-                    if (!$navbar.hasClass('is-pinned')) {
-                        $navbar.addClass('is-pinned');
-                        gsap.to($navbar, { y: -$navbar.outerHeight(), duration: 0.3, ease: "power2.out" });
+                if (Math.abs(scrollDiff) > threshold) {
+                    if (scrollDiff > 0 && currentY > 50) {
+                        // Scrolling down
+                        if (!$navbar.hasClass('is-pinned')) {
+                            $navbar.addClass('is-pinned');
+                            gsap.to($navbar, { y: -$navbar.outerHeight(), duration: 0.3, ease: "power2.out" });
+                        }
+                    } else if (scrollDiff < 0) {
+                        // Scrolling up
+                        if ($navbar.hasClass('is-pinned')) {
+                            $navbar.removeClass('is-pinned');
+                            gsap.to($navbar, { y: 0, duration: 0.3, ease: "power2.out" });
+                        }
                     }
-                } else if (currentY < lastY) {
-                    if ($navbar.hasClass('is-pinned')) {
-                        $navbar.removeClass('is-pinned');
-                        gsap.to($navbar, { y: 0, duration: 0.3, ease: "power2.out" });
-                    }
-                }
 
-                lastY = currentY;
+                    lastY = currentY;
+                }
             }
         });
     }
